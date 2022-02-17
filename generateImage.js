b3b94927173522644f22e12c88afa85618056bf7
MerkomassUnity <merkomass@gmail.com>
2022-02-17 23:45:46 +0100

    added an automatically generated  welcome message + welcome image and commented my code

---
+++ generateImage.js
@@ -0,0 +1,66 @@
+const Canvas = require("canvas");
+const Discord = require("discord.js")
+
+const background = "https://i.imgur.com/sMHCgKF.jpg" //link to background
+
+//const Roboto = require("@canvas-fonts/roboto")
+
+//  dimensions of background image
+//  !!! NEEDS TO BE CHANGED MANUALLY FOR EVERY PICTURE !!! (for now)
+const dim = {
+    height: 675,
+    width: 1200,
+    margin: 50,
+}
+
+const av = {
+    size:256,
+    x: 480,
+    y: 170
+}
+//generates the welcome picture with profile pic of user
+const generateImage = async (member) => {
+    let username = member.user.username //username
+    let discrim = member.user.discriminator
+    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size}) //saves user profile pic for a variable
+    const canvas = Canvas.createCanvas(dim.width, dim.height)
+    const ctx = canvas.getContext("2d")
+    
+    //draw in the background
+    const backimg = await Canvas.loadImage(background)
+    ctx.drawImage(backimg, 0, 0)
+    
+    //draw black tinted box for readability
+    ctx.fillStyle = "rgba(0,0,0,.8)"
+    ctx.fillRect(dim.margin, dim.margin, dim.width - 2 * dim.margin,dim.height - 2 * dim.margin)
+    
+    const avimg = await Canvas.loadImage(avatarURL) //load avatar picture
+    ctx.save() //save selection
+    
+    // circle around user avatar
+    ctx.beginPath()
+    ctx.arc(av.x + av.size / 2, av.y + av.size / 2, av.size / 2, 0, Math.PI * 2, true)
+    ctx.closePath()
+    ctx.clip()
+    
+    ctx.drawImage(avimg, av.x, av.y)
+    ctx.restore()
+    
+    //text style
+    ctx.fillStyle = "white"
+    ctx.textAlign = "center"
+    
+    ctx.font = "50px Roboto"
+    ctx.fillText("Welcome",dim.width / 2, dim.margin + 70)
+    
+    ctx.font = "60px Roboto"
+    ctx.fillText(username + discrim, dim.width / 2, dim.height - dim.margin - 125)
+    
+    ctx.font = "40px Roboto"
+    ctx.fillText("to the server", dim.width / 2, dim.height - dim.margin - 50)
+    
+    //convert to image
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    return attachment
+}
+module.exports = generateImage
--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 ﻿const Discord = require("discord.js")
-const {Message} = require("discord.js");
+//const {Messages} = require("mess")   no idea what it did lol
 require("dotenv").config()
+
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
+        //basically which permissions the bot uses
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
-
+//system check to write if logged in with correct user profile (bot)
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
+//basic message react 
 client.on("messageCreate", (message)=>{
-    if (message.content === "ahuj"){
-        message.reply("nazdaar")
+    if (message.content === "ahuj"){ //trigger word
+        message.reply("nazdaar") //answer
     }
 })
+//basic message react 
 client.on("messageCreate", (message)=>{
-    if (message.content === "koupíš mi kebab?"){
-        message.reply("ne")
+    if (message.content === "ban alex"){ //trigger word
+        message.reply("processing...") //answer
     }
 })
+//discord id for welcome channel
+const welcomeChannelID = "940643051867619388"
 
+//when a member joins, the bot sends a welcome message
+client.on("guildMemberAdd", async (member) =>{
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content: `<@${member.id}> Welcome to this server :)`,
+        files: [img]
+    })
+})
 
+//when a member leaves, the bot sends a leave message
+client.on("guildMemberRemove", (member) =>{
+    member.guild.channels.cache.get(welcomeChannelID).send(`<@${member.id}> bye :(`)
+})
+
+//logs the bot in with the discord token stored in .env file (not public, don't leak)
 client.login(process.env.TOKEN)
