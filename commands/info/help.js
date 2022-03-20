7ba0b7094aa88cb946788e384e20889ccac13d60
MerkomassUnity <merkomass@gmail.com>
2022-03-20 22:11:20 +0100

    added the avatar command into help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -13,6 +13,7 @@ module.exports = {
             .setDescription('Here are the commands i can do:')
             .addFields(
                 {name: 'Information', value: '?help - displays this message\n?links - send all the links where you can find me'},
+                {name: 'Users', value: '?avatar <@user> - displays avatar of mentioned user or user which ran the command'},
                 {name: 'Fun', value: 'none - you shall not have fun!'},
                 {name: 'Other', value: '?ping - replies with "pong"\n?secret - a very secret command'},
             )
