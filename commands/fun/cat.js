b096b357228205ec19facff5f60a7df01654a775
MerkomassUnity <merkomass@gmail.com>
2022-03-26 19:58:55 +0100

    added cat.js command

---
+++ commands/fun/cat.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+module.exports = {
+    name: "cat",
+    category: "fun",
+    description: "",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        //i`ll have to figure out how to make this into an embed
+        fetch('https://aws.random.cat/meow').then(response => response.json());
+                const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+                const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+                
+                message.reply({ files: [file] });
+            }
+}
