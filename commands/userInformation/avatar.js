0dc509dff2cdab70af5b8bfab1dab731418debcc
MerkomassUnity <merkomass@gmail.com>
2022-03-28 21:34:53 +0200

    fixed avatar.js not working when an user has no profile picture (default one)

--- commands/userInformation/avatar.js
+++ commands/userInformation/avatar.js
@@ -6,12 +6,12 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-            const user = message.mentions.users.first() || message.author;
+            const user = message.mentions.users.first() || message.author
             const avatarEmbed = new MessageEmbed()
                 .setColor(`#0099ff`)
                 .setTitle(`${user.username}'s Avatar`)
                 .setImage(
-                    `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=256`
+                    user.displayAvatarURL({size:256})
                 )
                 .setTimestamp()
                 .setFooter({text: 'MerkomassBot :)', iconURL: 'https://i.imgur.com/f5nZluu.jpg'})
