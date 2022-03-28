2239f3527d63c31d831d6181eccaee7e5676f55b
MerkomassUnity <merkomass@gmail.com>
2022-03-28 23:32:03 +0200

    commented code in cat.js

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -8,10 +8,10 @@ module.exports = {
     devOnly: false,
     run: async ({client, message, args}) => {
         //i`ll have to figure out how to make this into an embed
-        fetch('https://aws.random.cat/meow').then(response => response.json());
-                const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-                const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+        fetch('https://aws.random.cat/meow').then(response => response.json()); //calls the API and receives it in JSON format
+                const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json()); 
+                const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str); //makes sure the embed text doesn't go beyond 1024 characters
                 
-                message.reply({ files: [file] });
+                message.reply({ files: [file] }); //sends the cat image (file)
             }
 }
