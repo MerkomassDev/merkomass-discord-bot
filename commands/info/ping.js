bfc819175886a2b189cee292e387bb74f34a5d23
MerkomassUnity <merkomass@gmail.com>
2022-03-28 23:42:41 +0200

    commented code in ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -6,10 +6,10 @@ module.exports = {
     description: "measures ping of bot and api",
     run: async ({client, message, args}) => {
         
-            message.reply('Calculating ping...').then((resultMessage) => {
-                const ping = resultMessage.createdTimestamp - message.createdTimestamp
+            message.reply('Calculating ping...').then((resultMessage) => { //writes 'Calculating ping...'
+                const ping = resultMessage.createdTimestamp - message.createdTimestamp //calculates ping
 
-                resultMessage.edit(`Bot latency: ${ping}, API Latency: ${client.ws.ping}`)
+                resultMessage.edit(`Bot latency: ${ping}, API Latency: ${client.ws.ping}`) //edits the message to have corresponding numbers
             })
         },
 }
